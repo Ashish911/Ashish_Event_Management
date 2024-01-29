06bb948c867abfba44ee27625becb0cb4426d7ce
Ashish911 <ashishdongol32@gmail.com>
2024-01-29 13:53:52 +0000

    Learning clerk and how to use it

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+import qs from "query-string";
+
+import { UrlQueryParams, RemoveUrlQueryParams } from "@/types";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
