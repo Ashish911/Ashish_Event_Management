f4ffad1d2be8b933519da6d30655a811ecbd5927
Ashish911 <ashishdongol32@gmail.com>
2024-02-01 00:25:39 +0000

    Fixes

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -40,12 +40,7 @@ const Search = ({
 
   return (
     <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
-      <Image
-        src="/assets/icons/search.svg"
-        alt="search"
-        width={24}
-        height={24}
-      />
+      <Image src="/icons/search.svg" alt="search" width={24} height={24} />
       <Input
         type="text"
         placeholder={placeholder}
